a5701ae196ec9a68d88b5f8f53c488127bac72c0
Babashov Mahammad <babashov.info@gmail.com>
2023-01-19 01:36:59 +0400

    added FormikDemo created with useFormik hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import FormikDemo from './formik';
+// import FormikDemo from './formik';
+import UseFormik from './formik/UseFormik'
 
 
 function App() {
   return (
     <div className="App">
-       <FormikDemo/>
+       <UseFormik/>
     </div>
   );
 }
