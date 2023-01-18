0757a71dc8a99f9b3a6904da5c9aa299cc7440b2
Babashov Mahammad <babashov.info@gmail.com>
2023-01-19 01:33:33 +0400

    added to another jsx file

---
+++ src/formik/index.jsx
@@ -0,0 +1,77 @@
+import { Formik } from 'formik';
+
+import React from 'react'
+
+function FormikDemo() {
+  return (
+    <div>
+      <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        gender:'male',
+        hobbies:[],
+        country:"Azerbaijan"
+      }}
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+
+      {({handleSubmit,handleChange,values})=>(
+        
+        <>
+
+          <form onSubmit={handleSubmit}>
+
+          <label htmlFor="firstName">First Name</label>
+          <input onChange={handleChange} name="firstName" />
+          <br/>
+
+          <label htmlFor="lastName">Last Name</label>
+          <input onChange={handleChange} name="lastName" />
+          <br/>
+
+          <label htmlFor="email">Email</label>
+          <input onChange={handleChange} type="email" name="email" />
+          <br/><br/>
+
+
+           <input type="radio" name ="gender" value="male" checked={values.gender === "male" } onChange={handleChange} /> Male <br/>
+           <input type="radio" name="gender" value="female" checked={values.gender === "female" } onChange={handleChange} /> Female <br/>
+          <br/><br/>
+
+          <input type="checkbox" value="football" name="hobbies" onChange={handleChange} /> Football <br/>
+          <input type="checkbox" value="cinema" name="hobbies" onChange={handleChange} /> Cinema <br/>
+          <input type="checkbox" value="basketball" name="hobbies" onChange={handleChange} /> Basketball <br/>
+
+
+          <select name="country" value={values.country} onChange={handleChange}>
+            <option value="Azerbaijan">Azerbaijan</option>
+            <option value="Russian" >Russian</option>
+            <option value="English">English</option>
+          </select>
+
+          <br/><br/>
+          <button type="submit">Submit</button>
+          <br/><br/>
+          </form>
+
+          <code>
+            {JSON.stringify(values)}
+          </code>
+
+        </>
+
+      
+
+      )}
+      
+      
+</Formik>
+    </div>
+  )
+}
+
+export default FormikDemo
