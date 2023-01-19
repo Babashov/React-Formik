4b6f019376b06925e8d1fc0b9d966fbe69b832ec
Babashov Mahammad <babashov.info@gmail.com>
2023-01-19 15:16:33 +0400

    Custom Error messages

--- src/yupjs/index.js
+++ src/yupjs/index.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 const validationSchema = yup.object({
-    email: yup.string().nullable().email().required(),
-    password:yup.string().min(5).required(),
-    passwordConfirm:yup.string().oneOf([yup.ref('password')]).min(5).required()
+    email: yup.string().nullable().email('Email duzgun yazilmayib').required('Mutleq daxil olunmalidir'),
+    password:yup.string().min(5,'En az 5 herifden ibaret olmalidir').required('Mutleq daxil olunmalidir'),
+    passwordConfirm:yup.string().oneOf([yup.ref('password')],'Sifre uygun gelmir').min(5,'En az 5 herifden ibaret olmalidir').required('Mutleq daxil olunmalidir')
   });
 
 export default validationSchema;
