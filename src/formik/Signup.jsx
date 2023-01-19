423d8187a87297418c73d37c08c9997ff5c3e697
Babashov Mahammad <babashov.info@gmail.com>
2023-01-19 14:47:16 +0400

    Signup component with validationSchema

---
+++ src/formik/Signup.jsx
@@ -0,0 +1,58 @@
+import {useFormik} from 'formik'
+import validationSchema from '../yupjs';
+
+function Signup() {
+    const {handleSubmit,handleChange,values,errors} = useFormik({
+        initialValues:{
+            email: '',
+            password:'',
+            passwordConfirm:''
+          },
+          onSubmit:(values) => {
+            console.log(values);
+          },
+
+          validationSchema,
+      })
+
+  return (
+    <div>
+
+<div>
+        
+        <>
+
+          <form onSubmit={handleSubmit}>
+
+          <label htmlFor="email">Email</label>
+          <input onChange={handleChange} type="email" name="email" />
+          {errors.email && <div style={{color:'white',backgroundColor:'red'}}>{errors.email}</div>}
+          <br/><br/>
+
+          <label htmlFor="password">Password</label>
+          <input onChange={handleChange} type="password" name="password" />
+          {errors.password && <div style={{color:'white',backgroundColor:'red'}}>{errors.password}</div>}
+          <br/><br/>
+
+          <label htmlFor="passwordConfirm">Confirm password</label>
+          <input onChange={handleChange} type="password" name="passwordConfirm" />
+          {errors.passwordConfirm && <div style={{color:'white',backgroundColor:'red'}}>{errors.passwordConfirm}</div>}
+          <br/><br/>
+
+          <br/><br/>
+          <button type="submit">Submit</button>
+          <br/><br/>
+          </form>
+
+          <code>
+            {JSON.stringify(values)}
+          </code>
+
+        </>
+    </div>
+
+    </div>
+  )
+}
+
+export default Signup
